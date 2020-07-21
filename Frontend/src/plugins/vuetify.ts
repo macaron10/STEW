d16c4f454c62c0fc79e2f26f1b6296dc1c182cba
Min <macaron61@gmail.com>
2020-07-21 18:37:20 +0900

    [Fix (F)] 서버실행시 버전 호환 Issue 해결

--- Frontend/src/plugins/vuetify.ts
+++ Frontend/src/plugins/vuetify.ts
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-
+// const Vuetify = require("vuetify/lib");
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        options: {
-          customProperties: true,
-        },
-      },
+  theme: {
+    options: {
+      customProperties: true
+    }
+  }
 });

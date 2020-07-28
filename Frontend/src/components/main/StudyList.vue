df87379027c7b4bab5a904978f5e92300429a9be
Min <macaron61@gmail.com>
2020-07-28 16:12:27 +0900

    [Feat(F)] 스터디 리스트 UI 구성 Fixes S03P12B103-88

--- Frontend/src/components/main/StudyList.vue
+++ Frontend/src/components/main/StudyList.vue
@@ -1,40 +1,34 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <v-card>
-        <v-container fluid>
-          <v-row>
-            <v-col
-              v-for="group in groups"
-              :key="group.gpNo"
-              class="d-flex child-flex"
-              cols="3"
-            >
-              <v-card flat tile class="d-flex">
-                <v-img
-                  :src="`https://picsum.photos/500/300?image=1`"
-                  :lazy-src="`https://picsum.photos/10/6?image=1`"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-row>
-                  </template>
-                  <h4 class="white--text" >{{ group.gpNm }}</h4>
-                </v-img>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="group in groups"
+        :key="group.gpNo"
+        class="d-flex child-flex"
+        cols="3"
+      >
+        <v-card
+          class="mx-auto"
+          :to="{ name:'Home' }"
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+            height="150"
+          ></v-img>
+          <v-list-item>
+            <!-- 제목, 간단한설명, 태그, 썸네일, 인원수 -->
+            <v-list-item-content>
+              <v-list-item-title class="headline">{{ group.gpNm }}</v-list-item-title>
+              <v-list-item-subtitle>추가할거면 하고</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-card-text>
+            {{ group.gpIntro }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>

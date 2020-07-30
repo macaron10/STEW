44ad27cd8cfdb88c1f03d7beaf9dfe16af0b0fc7
HEEJIN <heehee2310@gmail.com>
2020-07-30 16:02:07 +0900

    [Feat (F)] 회원가입 유효성검사 일부 완료

--- Frontend/src/views/Signup.vue
+++ Frontend/src/views/Signup.vue
@@ -1,58 +1,62 @@
 <template>
     <div id="Signup">
-      <!-- <ValidationObserver ref="observer" v-slot="{ validate, reset }"> -->
-        <v-contaier>
-            <h1> 회원가입 </h1>
-            <form class="signup-form">
-              <!-- <ValidationProvider v-slot="{ errors }" name="Name" rules="required|max:10"> -->
+        <h1> 회원가입 </h1>
 
+        <v-row justify="center">
+          <v-col cols="12" sm="10" md="8" lg="6">
+            <v-form ref="form">
                 <v-text-field
-                    v-model="user.email" name="email"
-                    :error-messages="emailErrors"
-                    label="이메일"
+                    v-model="user.email"
+                    :rules="[
+                      () => !!user.email || '이메일을 입력해주세요.',
+                      () => /.+@.+\..+/.test(user.email) || '이메일 형식이 올바르지 않습니다.',
+                    ]"
+                    label="이메일 *"
+                    :readonly="idCheck"
+                    :clearable=false
                     required
-                    @input="$v.email.$touch()"
-                    @blur="$v.email.$touch()"
                 />
 
-                <v-btn>
+                  <!-- :disabled="!disableCheck" -->
+                <v-btn 
+                  @click="idCheckHandler" 
+                >
                   이메일 확인
                 </v-btn>
 
                 <v-text-field
-                    v-model="user.pwd" name="pwd"
-                    label="비밀번호"
+                    v-model="user.pwd"
+                    :rules="[
+                      () => !!user.pwd || '비밀번호를 입력해주세요.',
+                      () => user.pwd.length >= 6 || '6자 이상 입력해주세요.',
+                      addressCheck
+                    ]"
+                    label="비밀번호 *"
                     type="password"
+                    counter="15"
                     required
                 />
 
                 <v-text-field
-                  v-model="user.pwdCheck" name="pwdCheck"
-                  label="비밀번호 확인"
+                  v-model="user.pwdCheck"
+                  label="비밀번호 확인 *"
+                  :rules="[
+                    () => !!user.pwdCheck || '비밀번호를 재입력해주세요.',
+                    () => user.pwd === user.pwdCheck || '비밀번호와 일치하지 않습니다.'
+                  ]"
                   type="password"
+                  counter="15"
                   required
                 />
 
-                <!-- {{ computedCheckPwd }} -->
-
                 <v-text-field
-                    v-model="user.name" name="name"
-                    label="이름"
+                    v-model="user.name"
+                    :rules="[() => !!user.name || '이름을 입력해주세요.']"
+                    label="이름 *"
                     required
                 />
 
-                <v-text-field
-                    v-model="user.phone" name="phone"
-                    label="연락처"
-                    type="tel"
-                />
-
-                <!-- <v-divider></v-divider> -->
-
-                <v-radio-group v-model="user.gender" row>
-                    <v-radio class="user-gender" label="여자" value="F" />
-                    <v-radio class="user-gender" label="남자" value="M" />
-                </v-radio-group>
+                <v-subheader>선택사항</v-subheader>
 
                 <v-file-input 
                     v-model="user.img"
@@ -61,45 +65,33 @@
                     prepend-icon="mdi-camera"
                 ></v-file-input>
 
-                <v-text-field
+                <v-textarea
                     v-model="user.intro"
                     label="자기소개"
-                    :counter="100"
-                ></v-text-field>
+                    auto-grow rows="1" row-height="15"
+                    counter
+                    maxlength="100"
+                ></v-textarea>
 
                 <v-text-field
                     v-model="user.goalHr"
                     label="하루 목표 시간"
                     type="number"
+                    background-color="lightgray"
                 ></v-text-field>
 
-                <v-btn class="mr-4" @click="signupHandler">회원가입</v-btn>
-              <!-- </ValidationProvider> -->
-
-            </form>
-        </v-contaier>
-      <!-- </ValidationObserver> -->
+                <v-btn class="mr-4" @click="formCheckHandler">회원가입</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
   import axios from 'axios';
-  import VeeValidate, { Validator } from 'vee-validate';
-  import KoreanValidate from 'vee-validate/dist/locale/ko';
-
-  // Validator.localize('ko', KoreanValidate);
-  // Vue.use(VeeValidate, { locale: KoreanValidate })
-  
-  // extend('required', {
-  //   ...required,
-  //   message: '{_filed_} 를 입력해주세요'
-  // })
 
   export default {
-    components: {
-      // ValidationProvider,
-      // ValidationObserver,
-    },
+    components: {},
     data() {
       return {
         user: {
@@ -107,67 +99,78 @@
           pwd: '',
           pwdCheck: '',
           name: '',
-          phone: '',
-          gender: '',
           img: '',
           intro: '',
           goalHr: ''
         },
         idCheck: false,
-        pwdCheck: false,
       }
     },
     computed: {
-      // computedCheckPwd() {
-      //   if (this.user.pwd === this.user.pwdCheck) {
-      //     this.pwdCheck = true;
-      //     return "비밀번호가 확인되었습니다.";
-      //   } else {
-      //     return "비밀번호를 확인해주세요.";
-      //   }
-      // }
+      disableCheck() {
+        return this.$refs.form.$children[0].validate();
+      }
     },
     methods: {
-      formCheck() {
-        if (this.idCheck && this.pwdCheck) {
-          this.signupHandler();
+      idCheckHandler() {
+        axios.get('http://localhost:8399/api/user/check', {
+          params: {
+            email: this.user.email
+          }
+        })
+        .then( res => {
+          console.log(res);
+          if (res.msg === 'success' && res.object === 'true') {
+            this.idCheck = true;
+          }
+        })
+      },
+
+      formCheckHandler() {
+        console.log("체크핸들러에염");
+        console.log(this.$refs.form.validate());
+        console.log(this.$refs.form.$children[0].validate());
+
+        if (this.$refs.form.validate()) {
+          if (this.idCheck) {
+            this.signupHandler();
+          } else {
+            alert("이메일 확인을 눌러주세요!!")
+          }
+        } else {
+          alert("입력폼을 다시 확인해주세요!!");
         }
       },
 
       signupHandler() {
-        console.log(this.user);
+        // console.log(this.user);
         axios.post('http://localhost:8399/api/user/signup', {
           userEmail: this.user.email,
           userPw: this.user.pwd,
           userNm: this.user.name,
-          userPhone: this.user.phone,
-          userGender: this.user.gender,
           userImg: this.user.img,
           userIntro: this.user.intro,
           userGoalHr: this.user.goalHr,
-          roles: "USER"
         })
         .then(({ data }) => {
           let msg = '다시 시도해주세요';
-          if (data === 'success') {
+          console.log("로그인결과임다");
+          console.log(data);
+
+          if (data.msg === 'success') {
             msg = '회원가입 성공';
+            this.moveMain();
           }
           alert(msg);
-          this.moveMain();
         })
       },
+
       moveMain() {
         this.$router.push('/main');
       },
-      // submit () {
-      //   this.$refs.observer.validate()
-      // },
     },
   }
 </script>
 
 <style scoped>
-    #Signup {
-        margin: 50px 150px;
-    }
 </style>

345565bc111ef78b7b640e3ee45fd84d12b924fb
김송희 <girawhale@naver.com>
2020-08-03 15:10:07 +0900

    [Feat(B)] 간단한 에러처리 추가

--- Backend/src/main/java/com/ssafy/study/common/notification/MatterMostSender.java
+++ Backend/src/main/java/com/ssafy/study/common/notification/MatterMostSender.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 public class MatterMostSender {
 	private Logger log = LoggerFactory.getLogger(MatterMostSender.class);
 
-	private boolean mmEnabled = false;
+	private boolean mmEnabled = true;
 	private String webhookUrl = "https://meeting.ssafy.com/hooks/7mpjmu3ubi87pfo8eo6kuxkniy";
 
 	public void sendMessage(Attachment attach) {

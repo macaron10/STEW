d5388dc4d39175ebac988357ae195c1e35c30fe2
김송희 <girawhale@naver.com>
2020-08-07 10:37:03 +0900

    [Feat(B)] 소켓 진행중

--- Backend/src/main/java/com/ssafy/study/controller/SocketController.java
+++ Backend/src/main/java/com/ssafy/study/controller/SocketController.java
@@ -1,5 +1,16 @@
 package com.ssafy.study.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SocketController {
+	//receive를 받을 endpoint
+//	@MessageMapping("/receive")
+//	// /send로 메시지 반환
+//	@SendTo("/send")
+//	
 
 }

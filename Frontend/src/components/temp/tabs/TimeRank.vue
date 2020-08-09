04a3ee8c977266b980fa9da518e11f173cd5227b
HongJae Eom <ox1351@naver.com>
2020-08-09 21:38:07 +0900

    [Feat(F)] timer Rank기능 구현

--- Frontend/src/components/temp/tabs/TimeRank.vue
+++ Frontend/src/components/temp/tabs/TimeRank.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <span>이번달 우리 스터디의 공부량은 {{studyTotal}}입니다</span>
+    <p>이번달 우리 스터디의 공부량은 {{studyTotal}}입니다</p>
+    <p>순위</p>
+    <ol>
+      <li v-for="data in rankData" :key="data.user.userID">
+      시간:{{data.tmAcmlTime}} {{data.user.userNm}}님({{data.user.userEmail}})
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -12,13 +18,14 @@ export default {
   data() {
     return {
       gpNo: null,
-      studyTotal: "00:00:00"
+      studyTotal: "00:00:00",
+      rankData: null
     };
   },
   mounted() {
     this.gpNo = this.$route.params.id;
     this.getStudyTotal();
-    this.getStduyRank()
+    this.getStduyRank();
   },
   components: {},
   methods: {
@@ -30,12 +37,12 @@ export default {
             return;
           } else {
             const year = new Date().getFullYear();
-            const mon = new Date().getMonth() + 1
-            const month = String(mon).length === 2 ? mon : `0${mon}`
+            const mon = new Date().getMonth() + 1;
+            const month = String(mon).length === 2 ? mon : `0${mon}`;
             for (const i of res.data.object) {
               if (i.tmAcmlDate === `${year}-${month}`) {
-                this.studyTotal = i.tmAcmlTime
-                return
+                this.studyTotal = i.tmAcmlTime;
+                return;
               }
             }
           }
@@ -44,12 +51,16 @@ export default {
     },
     getStduyRank() {
       axios
-        .get(`/timer/user/${this.gpNo}/${new Date().getFullYear()}/${new Date().getMonth()+1}/`)
+        .get(
+          `/timer/user/${
+            this.gpNo
+          }/${new Date().getFullYear()}/${new Date().getMonth() + 1}/`
+        )
         .then(res => {
-          console.log(res)
+          this.rankData = res.data.object;
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
     }
-  },
+  }
 };
 </script>

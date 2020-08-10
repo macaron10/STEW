b7ed0dbb7f23e37e2cf57c3fcef8f1f40d06e622
Min <macaron61@gmail.com>
2020-08-10 12:05:39 +0900

    [Install(F)] 채팅 npm 설치

--- Frontend/src/store/index.ts
+++ Frontend/src/store/index.ts
@@ -132,6 +132,47 @@ const notifications = {
     }
   },
 }
+const chattings = {
+  namespaced: true,
+  state: {
+    chattings: []
+  },
+  mutations: {
+    setReqs(state: any, groupsReqs: any) {
+      state.groupsReqs = groupsReqs
+    }
+  },
+  actions: {
+    async getReqs({ state }: any, event: any) {
+      const apiUrl = '/study/user/reqlist'
+      try {
+        const res = await axios.get(apiUrl)
+        state.groupsReqs = res.data.object
+        console.log(res)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    // getChattingSock({ state, rootState }: any, gpNo: number) {
+    //   const apiUrl = rootState.baseUrl + '/sock'
+    //   const socket = new SockJS(apiUrl)
+    //   const ws = Stomp.over(socket)
+
+    //   const token = {
+    //     'accessToken': rootState.userInfo.accessToken
+    //   }
+    //   ws.connect(token,
+    //     frame => {
+    //       console.log('소켓 연결 성공');
+
+    //       ws.subscribe("/sub/chat/" + gpNo, msg =>{
+    //         state.groupsReqs.push(JSON.parse(msg.body))
+    //         console.log(JSON.parse(msg.body))
+    //       })
+    //     })
+    // },
+  },  
+}
 
 export default new Vuex.Store({
   state: {

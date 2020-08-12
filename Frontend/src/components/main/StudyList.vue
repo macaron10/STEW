d21d03ba958dcc86748fae67d0040ac294bef06e
김송희 <girawhale@naver.com>
2020-08-12 17:13:41 +0900

    [Feat(B)] Main UI 변경

--- Frontend/src/components/main/StudyList.vue
+++ Frontend/src/components/main/StudyList.vue
@@ -10,81 +10,92 @@
         sm="4"
         md="3"
       >
-          <!-- :to="'/study/' + group.gpNo" -->
-        <v-card
-          class="mx-auto"
-          @click="toDetail(group)"
-          height="270"
-        >
-          <v-img
-            :src="group.gpImg != null?($store.state.comm.baseUrl + '/image/group' + group.gpImg):gpImgDefault"
-            height="170"
-            color="white"
-            gradient="to bottom, rgba(255,255,255,0), rgba(0,0,0,.15)"
-          >
-            <v-row align="end" class="my-3 lightbox black--text pa-2 fill-height">
-              <v-col>
-                <div class="body-1 white--text"><v-icon color="white">mdi-account</v-icon>{{ group.gpCurNum }}/{{ group.gpMaxNum }}</div>
-              </v-col>
-            </v-row>
-          </v-img>
-          <v-list-item>
-            <!-- 제목, 간단한설명, 태그, 썸네일, 인원수 -->
-            <v-list-item-content>
-              <v-list-item-title>{{ group.gpNm }}</v-list-item-title>
-              <v-list-item-subtitle></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-card-text>
-            <span v-for="tag in group.gpTag" :key="tag">
-              # {{tag}}&nbsp;&nbsp;
-            </span>
-          </v-card-text>
-        </v-card>
+        <v-hover>
+          <template v-slot:default="{ hover }">
+            <!-- :to="'/study/' + group.gpNo" -->
+            <v-card class="mx-auto" height="250" @click="toDetail(group)">
+              <v-img
+                :src="group.gpImg != null?($store.state.comm.baseUrl + '/image/group' + group.gpImg):gpImgDefault"
+                height="170"
+                color="white"
+                gradient="to bottom, rgba(255,255,255,0), rgba(0,0,0,.15)"
+              >
+                <v-row>
+                  <v-col>
+                    <div class="body-1 white--text pl-2" align="start">
+                      <v-icon small color="#212121" v-if="!group.gpPublic">mdi-lock</v-icon>
+                      <v-icon small v-if="group.gpPublic"></v-icon>
+                    </div>
+                  </v-col>
+                </v-row>
+                <v-row align="center" class="mt-5 lightbox black--text pa-2"></v-row>
+                <v-row align="end" class="mt-9 lightbox black--text pa-2">
+                  <v-col>
+                    <div class="body-1 white--text">
+                      <v-icon color="white">mdi-account</v-icon>
+                      {{ group.gpCurNum }}/{{ group.gpMaxNum }}
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-img>
+              <v-list-item>
+                <!-- 제목, 간단한설명, 태그, 썸네일, 인원수 -->
+                <v-list-item-content>
+                  <v-list-item-title>{{ group.gpNm }}</v-list-item-title>
+                  <v-list-item-subtitle class="text-truncate">
+                    <v-chip
+                      class="ma-2"
+                      small
+                      color="#F5F5F5"
+                      v-for="tag in group.gpTag"
+                      :key="tag"
+                    ># {{tag}} &nbsp;&nbsp;</v-chip>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <!-- <v-card-text>
+              </v-card-text>-->
+              <v-fade-transition>
+                <v-overlay v-if="hover" absolute color="#424242">
+                  <div>
+                    <p class="multiwrap mx-auto">{{group.gpIntro}}</p>
+                    <div class="text-justify p-10 text-center mx-auto" style="width:80%">
+                      <span v-for="tag in group.gpTag" samll :key="tag">#{{tag}}&nbsp;&nbsp;</span>
+                    </div>
+                  </div>
+                  <!-- <v-btn
+                    align="end"
+                    color="#039BE5"
+                  >입장하기</v-btn> -->
+                </v-overlay>
+              </v-fade-transition>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
     </v-row>
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
+    <v-dialog v-model="dialog" width="500">
       <v-card>
-        <v-card-title class="headline indigo darken-1 white--text">
-          {{ selectedGroup.gpNm }} 에 가입하시겠습니까?
-        </v-card-title>
+        <v-card-title
+          class="headline blue accent-2 white--text"
+        >'{{ selectedGroup.gpNm }}' 에 가입하시겠습니까?</v-card-title>
 
-        <v-card-text class="py-1">
-         {{selectedGroup.gpIntro}}
-        </v-card-text>
+        <v-card-text class="py-1">{{selectedGroup.gpIntro}}</v-card-text>
 
         <v-divider></v-divider>
-        <v-textarea
-          v-model="message"
-          color="teal"
-          class="mx-5"
-        >
+        <v-textarea v-model="message" color="teal" class="mx-5">
           <template v-slot:label>
             <div class="px-5">
-              보낼 메세지 <small>(optional)</small>
+              보낼 메세지
+              <small>(optional)</small>
             </div>
           </template>
         </v-textarea>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="signUpGroup(selectedGroup.gpNo)"
-          >
-            가입신청
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            닫기
-          </v-btn>
+          <v-btn color="primary" text @click="signUpGroup(selectedGroup.gpNo)">가입신청</v-btn>
+          <v-btn color="primary" text @click="dialog = false">닫기</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -92,24 +103,24 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { mapState, mapActions } from 'vuex'
-import querystring from 'querystring'
+import axios from "axios";
+import { mapState, mapActions } from "vuex";
+import querystring from "querystring";
 
 export default {
-  name: 'StudyList',
-  data () {
+  name: "StudyList",
+  data() {
     return {
       myGroups: {},
       dialog: false,
       selectedGroup: {},
       snackbar: false,
       message: "",
-      gpImgDefault:this.$store.state.comm.baseUrl + "/image/group/default.png"
-    }
+      gpImgDefault: this.$store.state.comm.baseUrl + "/image/group/default.png"
+    };
   },
   methods: {
-    ...mapActions('sg', ['getGroups']),
+    ...mapActions("sg", ["getGroups"]),
     // 솔팅 함수
     // sortBy (prop) {
     //   if (prop === 'popularity') {
@@ -122,28 +133,28 @@ export default {
     //   this.sortedBy = prop
     //   this.movies.sort((a, b) => (a[prop] > b[prop] ? -1 : 1))
     // }
-    async getMyGroups () {
-      const apiUrl = '/study/user/my'
+    async getMyGroups() {
+      const apiUrl = "/study/user/my";
       try {
-        const res = await axios.get(apiUrl)
-        this.myGroups = res.data.object
+        const res = await axios.get(apiUrl);
+        this.myGroups = res.data.object;
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     },
     toDetail(group) {
-      this.selectedGroup = group
-      let flag = false
-      for (let i=0; i<this.myGroups.length; i++){
+      this.selectedGroup = group;
+      let flag = false;
+      for (let i = 0; i < this.myGroups.length; i++) {
         if (group.gpNo === this.myGroups[i].gpNo) {
-          flag = true
+          flag = true;
           // this.$router.push('/study/' + gpNo)
         }
       }
       if (flag === true) {
-        this.$router.push('/study/' + group.gpNo)
+        this.$router.push("/study/" + group.gpNo);
       } else {
-        this.dialog = true
+        this.dialog = true;
       }
     },
     // signUpGroup(gpNo) {
@@ -158,35 +169,52 @@ export default {
     //     })
     // },
     async signUpGroup(gpNo) {
-
-      const apiUrl = '/study/user/req?gpNo='+gpNo
+      const apiUrl = "/study/user/req?gpNo=" + gpNo;
       const msg = {
-        "reqMsg": this.message
-      }
+        reqMsg: this.message
+      };
       try {
-        const res = await axios.post(apiUrl, querystring.stringify({ reqMsg: this.message }))
-        console.log(res)
-        this.dialog = false
-        this.snackbar = true
+        const res = await axios.post(
+          apiUrl,
+          querystring.stringify({ reqMsg: this.message })
+        );
+        console.log(res);
+        this.dialog = false;
+        this.snackbar = true;
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     }
   },
   computed: {
     // groups () { return this.$store.state.sg.groups }
-    ...mapState('sg', ['groups'])
+    ...mapState("sg", ["groups"])
   },
-  mounted () {
-    this.getGroups()
+  mounted() {
+    this.getGroups();
     if (this.$store.state.auth.isLogin) {
-      this.getMyGroups()
+      this.getMyGroups();
     }
   }
-
-}
+};
 </script>
 
 <style>
-
+.multiwrap {
+  /* 한 줄 자르기 */
+  display: inline-block;
+  width: 80%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  /* 여러 줄 자르기 추가 스타일 */
+  white-space: normal;
+  line-height: 1.2;
+  height: 3.6em;
+  text-align: center;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
 </style>

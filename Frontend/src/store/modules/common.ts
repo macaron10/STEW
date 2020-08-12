6591bdd0041ea087c13c51ddf1aaea248ba002cb
김송희 <girawhale@naver.com>
2020-08-12 16:48:46 +0000

    Update Frontend/src/main.ts, Frontend/src/store/modules/common.ts, Frontend/src/views/MeetingRoom.vue, Backend/src/main/java/com/ssafy/study/controller/GroupController.java, Backend/src/main/java/com/ssafy/study/controller/UserController.java files

--- Frontend/src/store/modules/common.ts
+++ Frontend/src/store/modules/common.ts
@@ -1,7 +1,7 @@
 export default {
     state: {
-        baseUrl: "http://localhost:8399/api",//개발용
-        // baseUrl: "https://i3b103.p.ssafy.io/image", //배포용
+        // baseUrl: "http://localhost:8399/api",//개발용
+        baseUrl: "https://i3b103.p.ssafy.io", //배포용
         drawer: false,
       },
 

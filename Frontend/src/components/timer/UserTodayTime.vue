648aef18408b0a3d01697fdfb8bb268238199b59
HongJae Eom <ox1351@naver.com>
2020-08-14 17:14:45 +0900

    [Feat(F)] main작업ing....

--- Frontend/src/components/timer/UserTodayTime.vue
+++ Frontend/src/components/timer/UserTodayTime.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <v-row class="d-flex flex-column mx-auto">
-      <v-col md="6" sm="12">
+    <v-row class="d-flex mx-auto">
+      <v-col cols="12" sm="6">
         <v-card class="mx-auto" height="180px">
-          <v-card-title class="font-weight-bold">오늘의 공부시간</v-card-title>
-          <v-row justify="center" align="center" class="m-5">
-            <v-card-text
-              class="text--primary headline text-center"
-            >{{todayStudyTime.tmAcmlTime}} / {{userInfo.userGoalHr}}시간</v-card-text>
-          </v-row>
+          <v-card-title class="d-block font-weight-bold text-center">오늘의 공부시간</v-card-title>
+          <v-card-text
+            class="text--primary headline text-center text-subtitle-1 text-sm-h5 mt-7"
+          >{{todayStudyTime.tmAcmlTime}} / {{userInfo.userGoalHr}}시간</v-card-text>
         </v-card>
       </v-col>
-      <v-col  md="6" sm="12">
+
+      <v-col cols="12" sm="6">
         <v-card class="mx-auto" height="180px">
-          <v-card-title class="font-weight-bold">오늘의 목표 달성률</v-card-title>
-          <v-row justify="center" align="center" class="m-5">
+          <v-card-title class="d-block font-weight-bold text-center">오늘의 목표 달성률</v-card-title>
+          <div class="text-center">
             <v-progress-circular
               :indeterminate="indeterminate"
               :rotate="270"
@@ -23,7 +22,7 @@
               :width="15"
               color="light-blue"
             >{{todayStudyTime.tmAcmlTimeLong | toPercent(goalSecond)}} %</v-progress-circular>
-          </v-row>
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -53,7 +52,7 @@ export default {
       return percent > 100 ? 100 : percent;
     }
   },
-  mounted(){
+  mounted() {
     this.getUserInfo();
   },
   methods: {
--- Frontend/src/views/Main.vue
+++ Frontend/src/views/Main.vue
@@ -1,43 +1,34 @@
 <template>
   <div id="Mainpage">
     <MyPage />
-    <div class="p-3 ma-5" v-if="$store.state.auth.isLogin">
-      <v-row>
-        <v-col lg="6" sm="12">
-          <TodayTimer />
-        </v-col>
-        <v-col lg="3" sm="6">
-          <v-card class="mx-auto" height="180px" color="#42A5F5" dark>
-            <v-card-title class="font-weight-bold">나의 다짐</v-card-title>
-            <v-row justify="center" align="center" class="m-5">
-              <v-card-text class="h3 headline text-center ">{{userIntro}}</v-card-text>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
-    <br />
     <div class="p-3 ma-5">
-      <h2 class="mb-3">이달의 스터디 랭킹</h2>
       <div>
+        <h2 class="mb-3 text-center text-sm-left">{{new Date().getMonth()+1}}월의 스터디 랭킹</h2>
         <div v-if="rankGpList.length == 0">아직 이달의 랭킹이 존재하지 않습니다!</div>
-        <v-card flat tile class="d-flex">
+        <v-card flat tile class="d-flex justify-center justify-sm-start">
           <v-card flat tile v-for="(gp, index) in rankGpList" :key="index + gp" class="align-start">
-            <v-container>
+            <v-container v-bind:class="{ 'd-none d-sm-flex' : index > 2}">
               <v-row justify="space-between">
-                <v-col cols="auto" v-if="index < 3">
+                <v-col
+                  cols="4"
+                  offset="4"
+                  sm="4"
+                  offset-sm="0"
+                  class="pa-0 pa-sm-3"
+                  v-if="index < 3"
+                >
                   <v-img
                     :src="baseUrl + `image/main/${index+1}-medal.png`"
                     height="40px"
                     width="40px"
                   ></v-img>
                 </v-col>
-                <v-col cols="auto" v-else></v-col>
-                <v-col cols="auto" class="text-center pl-0">
+                <v-col cols="auto" class="pa-auto" v-else></v-col>
+                <v-col cols="12" sm="auto" class="text-center pl-0">
                   <v-row class="flex-column ma-0 pa-0" justify="center">
                     <v-col class="pa-0 ma-0">
                       <span class="blue--text">{{index+1}}등</span>&nbsp;
-                      <a color="black" >{{gp.gpNm}}</a>
+                      <a color="black">{{gp.gpNm}}</a>
                     </v-col>
                     <v-col class="pa-0 ma-0">
                       <span class="subtitle-2">
@@ -53,6 +44,23 @@
         </v-card>
       </div>
     </div>
+    <div class="p-3 ma-5" v-if="$store.state.auth.isLogin">
+      <v-row>
+        <v-col cols="12">
+          <TodayTimer />
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" height="180px" color="#42A5F5" dark>
+            <v-card-title class="font-weight-bold">나의 다짐</v-card-title>
+            <v-row justify="center" align="center" class="m-5">
+              <v-card-text class="h3 headline text-center">{{userIntro}}</v-card-text>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <br />
+
     <br />
     <div class="p-3 mx-5">
       <h2>스터디 목록</h2>
@@ -80,7 +88,7 @@ export default {
   },
   data: () => ({
     rankGpList: [],
-    userIntro : "",
+    userIntro: "",
     // baseUrl: "http://localhost:8399/api/"
     baseUrl: "https://i3b103.p.ssafy.io/"
     // baseUrl: this.$store.state.comm.baseUrl
@@ -109,9 +117,10 @@ export default {
   },
   methods: {
     getUserInfo() {
-      axios.get("/user/").then(({ data }) => {
-        this.userIntro = data.object.userIntro;
-      });
+      if (this.$store.state.auth.isLogin)
+        axios.get("/user/").then(({ data }) => {
+          this.userIntro = data.object.userIntro;
+        });
     },
     getRankGpList() {
       axios

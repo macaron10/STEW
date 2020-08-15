262c64032714859e276e889d583e2b0c7b6483f0
김송희 <girawhale@naver.com>
2020-08-15 20:36:57 +0900

    [Feat(F,B)] 채팅 Redis적용. 참가자 명단 등록

--- Backend/src/main/java/com/ssafy/study/chat/model/ChatMessage.java
+++ Backend/src/main/java/com/ssafy/study/chat/model/ChatMessage.java
@@ -1,10 +1,12 @@
 package com.ssafy.study.chat.model;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ssafy.study.user.model.UserDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,18 +20,18 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 public class ChatMessage {
-	private SimpleDateFormat sdf;
-
 	public enum MessageType {
 		ENTER, TALK, QUIT
 	}
 
+	@JsonIgnore
+	private SimpleDateFormat sdf;
+
 	private MessageType type;
-	private long gpNo;
+	private String gpNo;
 	private long userId;
 	private String userNm;
 	private String userImg;
-
 	private String chatMsg;
 
 	private String regTime;
@@ -40,5 +42,19 @@ public class ChatMessage {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 		this.regTime = sdf.format(date);
+	}
+
+	public ChatMessage(UserDto user) {
+		this();
+		this.userId = user.getUserId();
+		this.userImg = user.getUserImg();
+		this.userNm = user.getUserNm();
+	}
+
+	public ChatMessage(UserDto user, String gpNo, MessageType type) {
+		this(user);
+
+		this.gpNo = gpNo;
+		this.type = type;
 	}
 }
--- Frontend/src/components/chat/Chat.vue
+++ Frontend/src/components/chat/Chat.vue
@@ -1,5 +1,5 @@
 <template>
-  <div height="">
+  <div height>
     <ChatMemberList :members="members" />
     <MessageList class="msg-list" :msgs="messages" />
     <v-divider></v-divider>
@@ -8,74 +8,77 @@
 </template>
 
 <script>
-import { mapMutations, mapState } from 'vuex';
-import MessageList from '@/components/chat/comp/MessageList.vue';
-import MessageForm from '@/components/chat/comp/MessageForm.vue';
-import ChatMemberList from '@/components/chat/comp/ChatMemberList.vue';
+import { mapMutations, mapState } from "vuex";
+import MessageList from "@/components/chat/comp/MessageList.vue";
+import MessageForm from "@/components/chat/comp/MessageForm.vue";
+import ChatMemberList from "@/components/chat/comp/ChatMemberList.vue";
 // import Constant from '@/Constant';
 
-import Stomp from 'webstomp-client';
-import SockJS from 'sockjs-client';
+import Stomp from "webstomp-client";
+import SockJS from "sockjs-client";
+import axios from "axios";
+import { Cipher } from "crypto";
 
 export default {
-  name: 'Chat',
+  name: "Chat",
   data() {
     return {
       messages: [],
       roomid: 0,
-      members: [],
+      members: []
     };
   },
   components: {
     MessageList,
     MessageForm,
-    ChatMemberList,
+    ChatMemberList
   },
   methods: {
-      initSock () {
-        const apiUrl = this.$store.state.comm.baseUrl + '/api/sock'
-        const socket = new SockJS(apiUrl)
-        const ws = Stomp.over(socket)
-        this.ws = ws
-      },
-      getChattingSock(gpNo) {
-        const token = {
-          'accessToken': this.$store.state.auth.userInfo.accessToken
-        }
-        this.ws.connect(token,
-          frame => {
-            console.log('소켓 연결 성공');
-            this.ws.subscribe("/sub/chat/" + gpNo, msg =>{
-              // state.groupsReqs.push(JSON.parse(msg.body))
-              this.messages.push(JSON.parse(msg.body))
-              if (JSON.parse(msg.body).type == "ENTER") {
-                const userInf={
-                  "userNm": JSON.parse(msg.body).userNm,
-                  "userImg": JSON.parse(msg.body).userImg
-                }
-                this.members.push(userInf)
-              } else if (JSON.parse(msg.body).type == "QUIT") {
-                const itemToFind = this.members.find(function(item) {return item.userNm === JSON.parse(msg.body).userNm})
-                const idx = this.members.indexOf(itemToFind)
-                if (idx >-1) this.members.splice(idx, 1)
-              }
-              console.log(JSON.parse(msg.body), '구독msg')
-            })
-        this.sendEnterMsg()
-        })
-      },
+    initSock() {
+      const apiUrl = this.$store.state.comm.baseUrl + "/sock";
+      const socket = new SockJS(apiUrl);
+      const ws = Stomp.over(socket);
+      this.ws = ws;
+    },
+    getChattingSock(gpNo) {
+      const token = {
+        accessToken: this.$store.state.auth.userInfo.accessToken
+      };
+      this.ws.connect(token, frame => {
+        console.log("소켓 연결 성공");
+        this.ws.subscribe("/sub/chat/" + gpNo, msg => {
+          // state.groupsReqs.push(JSON.parse(msg.body))
+          this.messages.push(JSON.parse(msg.body));
+          if (JSON.parse(msg.body).type == "ENTER") {
+            const userInf = {
+              userNm: JSON.parse(msg.body).userNm,
+              userImg: JSON.parse(msg.body).userImg
+            };
+            this.members.push(userInf);
+          } else if (JSON.parse(msg.body).type == "QUIT") {
+            const itemToFind = this.members.find(function(item) {
+              return item.userNm === JSON.parse(msg.body).userNm;
+            });
+            const idx = this.members.indexOf(itemToFind);
+            if (idx > -1) this.members.splice(idx, 1);
+          }
+          console.log(JSON.parse(msg.body), "구독msg");
+        });
+        // this.sendEnterMsg();
+      });
+    },
     sendMessage(msg) {
       const token = {
-          'accessToken': this.$store.state.auth.userInfo.accessToken,
-      }
+        accessToken: this.$store.state.auth.userInfo.accessToken
+      };
       const tempMsg = {
-        type: 'TALK',
+        type: "TALK",
         gpNo: this.roomid,
         chatMsg: msg
-      }
-      const stringMsg = JSON.stringify(tempMsg)
+      };
+      const stringMsg = JSON.stringify(tempMsg);
       // this.messages.push(message)
-      this.ws.send("/pub/chat", stringMsg, token)
+      this.ws.send("/pub/chat", stringMsg, token);
     },
     sendEnterMsg() {
       //멤버에 나 추가
@@ -85,50 +88,68 @@ export default {
       // }
       // this.members.push({userInformation})
       const token = {
-        'accessToken': this.$store.state.auth.userInfo.accessToken
-      }
+        accessToken: this.$store.state.auth.userInfo.accessToken
+      };
       const enterMsg = {
-        type: 'ENTER',
+        type: "ENTER",
         gpNo: this.roomid,
         chatMsg: "입장했습니다."
-      }
-      const stringMsg = JSON.stringify(enterMsg)
+      };
+      const stringMsg = JSON.stringify(enterMsg);
       // this.messages.push(message)
-      this.ws.send("/pub/chat", stringMsg, token)
+      this.ws.send("/pub/chat", stringMsg, token);
     },
     sendQuitMsg() {
-        const token = {
-          'accessToken': this.$store.state.auth.userInfo.accessToken
-        }
+      const token = {
+        accessToken: this.$store.state.auth.userInfo.accessToken
+      };
       const quitMsg = {
-        type: 'QUIT',
+        type: "QUIT",
         gpNo: this.roomid,
         chatMsg: "님이 퇴장했습니다.(유저네임 들어오면 수정)"
-      }
-      const stringMsg = JSON.stringify(quitMsg)
+      };
+      const stringMsg = JSON.stringify(quitMsg);
       // this.messages.push(message)
-      this.ws.send("/pub/chat", stringMsg, token)
+      this.ws.send("/pub/chat", stringMsg, token);
     },
+    async getChatRoomInfo() {
+      const apiUrl = "/chatroom-info/" + this.$route.params.id;
+      try {
+        const res = await axios.get(apiUrl);
+        const userList = res.data.object.userList;
+
+        Object.values(userList).forEach(e => {
+          this.members.push(e);
+        });
+        console.log("##################################################33");
+        console.log(this.members);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  },
+  created() {
+    this.roomid = Number(this.$route.params.id);
+    this.getChatRoomInfo();
   },
   mounted() {
-    this.roomid= Number(this.$route.params.id)
-    this.initSock()
-    this.getChattingSock(this.roomid)
+    this.initSock();
+    this.getChattingSock(this.roomid);
   },
   destroyed() {
-    this.sendQuitMsg()
+    this.ws.disconnect();
+    this.sendQuitMsg();
   }
-  
 };
 </script>
 
 <style>
 /* .msg-form { */
-  /* position:absolute;
+/* position:absolute;
   right:0;
   bottom:0; */
 /* } */
 /* .msg-list { */
-  /* overflow-y: scroll; */
-/* } */ 
+/* overflow-y: scroll; */
+/* } */
 </style>
